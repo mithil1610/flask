b7183a2f455304535aede82af87cc2186112642c
Mithilkumar Shashikant Patel <mithilpatel140@gmail.com>
2022-04-18 01:52:36 -0500

    final commit before changes to code

--- app.py
+++ app.py
@@ -1,42 +1,63 @@
-from email import header
+'''
+Goal of Flask Microservice:
+1. Flask will take the repository_name such as angular, angular-cli, material-design, D3 from the body of the api sent from React app and 
+   will utilize the GitHub API to fetch the created and closed issues. Additionally, it will also fetch the author_name and other 
+   information for the created and closed issues.
+2. It will use group_by to group the data (created and closed issues) by month and will return the grouped data to client (i.e. React app).
+3. It will then use the data obtained from the GitHub API (i.e Repository information from GitHub) and pass it as a input request in the 
+   POST body to LSTM microservice to predict and forecast the data.
+4. The response obtained from LSTM microservice is also return back to client (i.e. React app).
+
+Use Python/GitHub API to retrieve Issues/Repos information of the past 1 year for the following repositories:
+- https: // github.com/angular/angular
+- https: // github.com/angular/material
+- https: // github.com/angular/angular-cli
+- https: // github.com/d3/d3
+'''
+# Import all the required packages 
 import os
 from flask import Flask, jsonify, request, make_response, Response
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 import json
-from github3 import login
 import dateutil.relativedelta
 from dateutil import *
-from datetime import date, timedelta
+from datetime import date
 import pandas as pd
-import time
 import requests
 
+# Initilize flask app
 app = Flask(__name__)
+# Handles CORS (cross-origin resource sharing)
 CORS(app)
 
-
+# Add response headers to accept all types of  requests
 def build_preflight_response():
     response = make_response()
     response.headers.add("Access-Control-Allow-Origin", "*")
     response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-    response.headers.add("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+    response.headers.add("Access-Control-Allow-Methods",
+                         "PUT, GET, POST, DELETE, OPTIONS")
     return response
 
-
+# Modify response headers when returning to the origin
 def build_actual_response(response):
     response.headers.set("Access-Control-Allow-Origin", "*")
-    response.headers.set("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+    response.headers.set("Access-Control-Allow-Methods",
+                         "PUT, GET, POST, DELETE, OPTIONS")
     return response
 
-
+'''
+API route path is  "/api/forecast"
+This API will accept only POST request
+'''
 @app.route('/api/github', methods=['POST'])
 def github():
-    # repo_name = "angular/angular"
     body = request.get_json()
+    # Extract the choosen repositories from the request
     repo_name = body['repository']
     # Add your own GitHub Token to run it local
     token = os.environ.get(
-        'GITHUB_TOKEN', 'your GitHub Token')
+        'GITHUB_TOKEN', 'YOUR_GITHUB_TOKEN')
     GITHUB_URL = f"https://api.github.com/"
     headers = {
         "Authorization": f'token {token}'
@@ -45,35 +66,44 @@ def github():
         "state": "open"
     }
     repository_url = GITHUB_URL + "repos/" + repo_name
+    # Fetch GitHub data from GitHub API
     repository = requests.get(repository_url, headers=headers)
+    # Convert the data obtained from GitHub API to JSON format
     repository = repository.json()
-    # github = login(token=token)
 
     today = date.today()
 
     issues_reponse = []
+    # Iterating to get issues for every month for the past 12 months
     for i in range(12):
         last_month = today + dateutil.relativedelta.relativedelta(months=-1)
         types = 'type:issue'
         repo = 'repo:' + repo_name
         ranges = 'created:' + str(last_month) + '..' + str(today)
+        # By default GitHub API returns only 30 results per page
+        # The maximum number of results per page is 100
+        # For more info, visit https://docs.github.com/en/rest/reference/repos 
+        per_page = 'per_page=100'
+        # Search query will create a query to fetch data for a given repository in a given time range
         search_query = types + ' ' + repo + ' ' + ranges
-        
-        query_url = GITHUB_URL + "search/issues?q=" + search_query
-        # search_issues = github.search_issues(search_query)
+
+        # Append the search query to the GitHub API URL 
+        query_url = GITHUB_URL + "search/issues?q=" + search_query + "&" + per_page
+        # requsets.get will fetch requested query_url from the GitHub API
         search_issues = requests.get(query_url, headers=headers, params=params)
+        # Convert the data obtained from GitHub API to JSON format
         search_issues = search_issues.json()
         issues_items = []
         try:
+            # Extract "items" from search issues
             issues_items = search_issues.get("items")
         except KeyError:
             error = {"error": "Data Not Available"}
             resp = Response(json.dumps(error), mimetype='application/json')
             resp.status_code = 500
             return resp
-            
-        
-            
+        if issues_items is None:
+            continue
         for issue in issues_items:
             label_name = []
             data = {}
@@ -101,13 +131,16 @@ def github():
 
     df = pd.DataFrame(issues_reponse)
 
-    # Daily Created At
+    # Daily Created Issues
     df_created_at = df.groupby(['created_at'], as_index=False).count()
     dataFrameCreated = df_created_at[['created_at', 'issue_number']]
     dataFrameCreated.columns = ['date', 'count']
 
-    # Monthly Created At
-    created_at = df['created_at'].sort_values(ascending=True)
+    '''
+    Monthly Created Issues
+    Format the data by grouping the data by month
+    ''' 
+    created_at = df['created_at']
     month_issue_created = pd.to_datetime(
         pd.Series(created_at), format='%Y/%m/%d')
     month_issue_created.index = month_issue_created.dt.to_period('m')
@@ -120,7 +153,11 @@ def github():
         array = [str(key), month_issue_created_dict[key]]
         created_at_issues.append(array)
 
-    # Monthly Closed At
+    '''
+    Monthly Closed Issues
+    Format the data by grouping the data by month
+    ''' 
+    
     closed_at = df['closed_at'].sort_values(ascending=True)
     month_issue_closed = pd.to_datetime(
         pd.Series(closed_at), format='%Y/%m/%d')
@@ -142,24 +179,41 @@ def github():
     '''
     created_at_body = {
         "issues": issues_reponse,
-        "type": "created_at"
+        "type": "created_at",
+        "repo": repo_name.split("/")[1]
     }
     closed_at_body = {
         "issues": issues_reponse,
-        "type": "closed_at"
+        "type": "closed_at",
+        "repo": repo_name.split("/")[1]
     }
-    
-    # Update your gcloud lstm url
-    
-    LSTM_API_URL = "lstm_gcloud_url/" + "api/forecast"
 
+    # Update your Google cloud deployed LSTM app URL (NOTE: DO NOT REMOVE "/")
+    LSTM_API_URL = "https://lstm-forecast-tqzys7bsda-uc.a.run.app/" + "api/forecast"
+
+    '''
+    Trigger the LSTM microservice to forecasted the created issues
+    The request body consists of created issues obtained from GitHub API in JSON format
+    The response body consists of Google cloud storage path of the images generated by LSTM microservice
+    '''
     created_at_response = requests.post(LSTM_API_URL,
-                                            json=created_at_body,
-                                            headers={'content-type': 'application/json'})
+                                        json=created_at_body,
+                                        headers={'content-type': 'application/json'})
+    
+    '''
+    Trigger the LSTM microservice to forecasted the closed issues
+    The request body consists of closed issues obtained from GitHub API in JSON format
+    The response body consists of Google cloud storage path of the images generated by LSTM microservice
+    '''    
     closed_at_response = requests.post(LSTM_API_URL,
-                                            json=closed_at_body,
-                                            headers={'content-type': 'application/json'})
-
+                                       json=closed_at_body,
+                                       headers={'content-type': 'application/json'})
+    
+    '''
+    Create the final response that consists of:
+        1. GitHub repository data obtained from GitHub API
+        2. Google cloud image urls of created and closed issues obtained from LSTM microservice
+    '''
     json_response = {
         "created": created_at_issues,
         "closed": closed_at_issues,
@@ -172,10 +226,10 @@ def github():
             **closed_at_response.json(),
         },
     }
-
+    # Return the response back to client (React app)
     return jsonify(json_response)
 
 
-# run server
+# Run flask app server on port 5000
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- readme.txt
+++ readme.txt
@@ -10,46 +10,54 @@ Step 1: What will Flask do?
 
 Step 2: Deploying Flask to gcloud platform
        1: You must have Docker(https://www.docker.com/get-started) and Google Cloud SDK(https://cloud.google.com/sdk/docs/install) 
-           installed on your computer. Then, Create a gcloud project for flask and enable the billing account.
+           installed on your computer. Then, Create a gcloud project and enable the following:
+           a.billing account
+           b.Conatiner Registry API
+           c. Cloudbuild API
 
-       2: Type `docker` on cmd terminal and press enter to get all required information
+       2. Copy the LSTM project url from gcloud and paste it in app.js file line 192 where it is written "your_lstm_gcloud_url/"
+       (NOTE: DO NOT REMOVE "/")   
 
-       3: Type `docker build .` on cmd to build a docker image
+       3: Type `docker` on cmd terminal and press enter to get all required information
 
-       4: Type `docker images` on cmd to see our first docker image. After hitting enter, newest created image will be always on the top of the list
+       4: Type `docker build .` on cmd to build a docker image
 
-       5: Now type `docker tag <your newest image id> gcr.io/<your project-id>/<project-name>` and hit enter 
+       5: Type `docker images` on cmd to see our first docker image. After hitting enter, newest created image will be always on the top of the list
+
+       6: Now type `docker tag <your newest image id> gcr.io/<your project-id>/<project-name>` and hit enter 
             Type `docker images` to see your image id updated with tag name
 
-       6: Type `gcloud init` on cmd
+       7: Type `gcloud init` on cmd and it will prompt Create or select a configuration choose existing configurations and hit enter and
+          it will prompt Choose a current Google Cloud project, choose your current gcloud project number and hit enter.
+          
+       8: Type `gcloud auth configure-docker` on cmd hit enter and then type "docker images"
 
-       7: Type `gcloud auth configure-docker` on cmd
+       9: Type `docker push <your newest created tag>` on cmd and hit enter
 
-       8: Go to your GCloud account and open container registry
+       10: Generate a Personal Access Token on GitHub by following below steps:
+          a. Navigate to your Git account settings, then Developer Settings. Click the Personal access tokens menu, then click Generate new token.
+          b. Select repo and public as the scope as shown in recording. 
+          c. Click Generate Token.
+          d. Copy the generated token and paste it in your sticky notes
 
-       9: Enable your billing account
-
-       10: Enable your Container Registry API
-
-       11: Go to the Cloud Build and enable Cloud Build API
-
-       12: Type `docker push <your newest created tag>` on cmd and hit enter
-
-       13: Generate GitHub Access token and save it somewhere.
-
-       14: Go to cloud run and create new service, service name will be your GCloud project name and for container image url 
-            hit select and selects your latest id and hit select and edit container port to '5000', increase the memory limit 
-            to 1GiB and go to variables and secrets and click Environment variable and type as follow.
+       11 Go to container registry you will see your newly pushed docker image, click on that docker image, after clicking, you will be able to see
+          docker image id and on the right side you will see "⋮", left click on "⋮" there you will be able to see option "deploy it to cloud run" click on that and 
+          it will navigate you to cloud run where in container image url hit select and select your latest id and change the Min Instance to 1 instead of 0 and the option 
+          to allow unauthorized access when creating new service and then go to container tab and edit container port to '5000', increase the memory limit 
+          to 1GiB and go to variable and secrets tab and click on add environment variable as follows
                Name                     value
-           a. GITHUB_TOKEN              "Your GitHub access token"
+           a. GITHUB_TOKEN              "Your GitHub generated token"
 
-       15: Click on create, this will create the service on port 5000 and will generate the url, hit the url.
+       12: Click on create, this will create the service on port 5000 and will generate the url, hit the url.
+
+       13: Copy flask gcloud generated url and paste in it sticky notes.
        
 
 Step 3: To run locally:
-       1. In app.py, at line 43, add your GitHub token
+       1. In app.py, at line 60, add your GitHub token
        2. Go to cmd terminal and type following:
         a. python -m venv env
         b. env\Scripts\activate.bat
         c. pip install -r requirements.txt
+        d. change the url of LSTM of file app.py (line 192) http://localhost:8080/
         d. python app.py
