fb40eee8600d753173fa649e79349ca219480b41
Mithilkumar Shashikant Patel <mithilpatel140@gmail.com>
2022-04-22 16:48:34 -0500

    changes to files

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def github():
             array = [str(key), month_issue_closed_dict[key]]
             closed_at_issues.append(array)
 
-    repository_url = GITHUB_URL + "repos/" + 'angular/angular' +'/pulls?state=created'
+    repository_url = GITHUB_URL + "repos/" + repo_name +'/pulls?state=created'
     r = requests.get(repository_url, headers=headers)
     pulls_response = r.json()
     another_page = True
@@ -226,9 +226,9 @@ def github():
                                        json=closed_at_body,
                                        headers={'content-type': 'application/json'})
     
-    # pulls_response_response = requests.post("https://lstm-forecast-tqzys7bsda-uc.a.run.app/api/pulls",
-    #                                    json=pulls_response_body,
-    #                                    headers={'content-type': 'application/json'})
+    pulls_response_response = requests.post("https://lstm-forecast-tqzys7bsda-uc.a.run.app/api/pulls",
+                                       json=pulls_response_body,
+                                       headers={'content-type': 'application/json'})
     
     '''
     Create the final response that consists of:
@@ -347,6 +347,9 @@ def github():
         },
         "closedAtImageUrls": {
             **closed_at_response.json(),
+        },
+        "pullsImageUrls": {
+            **pulls_response_response.json(),
         },
         "total_issues": total_issues,
         "stars_count": stars_count,
